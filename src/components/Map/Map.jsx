48a3df33bf14850e434d9b83851b1fdf59d1a76a
Aditria Pardana <aditria.pardana20@gmail.com>
2023-10-26 14:45:43 +0700

    update maps section

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import LocationOnOutlined from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
+import useStyles from "./styles";
 
 const Map = () => {
-  return (
-    <div>
-        <h1>Map</h1>
-    </div>
-  )
-}
+  const classes = useStyles();
+  const isMobile = useMediaQuery("(min-width:600px)");
+  const coordinates = { lat: 0, lng: 0 };
 
-export default Map
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBVDwu_qvivX46OsyOPjE1gFjwUcM2NTMo" }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={""}
+        onChange={""}
+        onChildClick={""}
+      ></GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
