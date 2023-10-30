aae15454842a97cebf2e897a11a9698df234f71c
Aditria Pardana <aditria.pardana20@gmail.com>
2023-10-30 14:40:27 +0700

    displace place on the map

--- src/App.js
+++ src/App.js
@@ -46,6 +46,7 @@ const App = () => {
             setCoordinates={setCoordinates}
             setBounds={setBounds}
             coordinates={coordinates}
+            places={places}
           />
         </Grid>
       </Grid>
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,9 +5,9 @@ import LocationOnOutlined from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab";
 import useStyles from "./styles";
 
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
   const classes = useStyles();
-  const isMobile = useMediaQuery("(min-width:600px)");
+  const isDesktop = useMediaQuery("(min-width:600px)");
 
   return (
     <div className={classes.mapContainer}>
@@ -26,7 +26,41 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
           });
         }}
         onChildClick={""}
-      ></GoogleMapReact>
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlined color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  alt={place.name}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                  }
+                />
+
+                <Rating size="small" value={Number(place.rating)} readOnly />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   );
 };
