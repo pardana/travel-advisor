d7acf6504b4ccca0d58a594667df1c641ed73fdf
Aditria Pardana <aditria.pardana20@gmail.com>
2023-10-30 10:29:51 +0700

    update section List

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const PlaceDetails = () => {
+const PlaceDetails = ({ place }) => {
   return (
     <div>
-        <h1>PlaceDetails</h1>
+      <h1>{place.name}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default PlaceDetails
+export default PlaceDetails;
